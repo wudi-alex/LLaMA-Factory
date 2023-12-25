c606702e3d19a54ae8d9ee2ad7bc41e86d47a281
dw <1798827828@qq.com>
2023-12-25 05:32:47 -0500

    fix

--- src/test.sh
+++ src/test.sh
@@ -22,4 +22,24 @@ nvidia-smi
 module load gnu10
 module load python
 
-python myscript.py
+accelerate launch --config_file accelerate_ds_config.yaml train_bash.py \
+    --stage rm \
+    --do_train \
+    --model_name_or_path codellama/CodeLlama-7b-hf \
+    --dataset apr_rm \
+    --dataset_dir /projects/ksun3/dwu25/apr_datasets_processing/coconut/data \
+    --template default \
+    --finetuning_type lora \
+    --lora_target q_proj,v_proj \
+    --lora_rank 32 \
+    --output_dir $OUTPUT/output \
+    --per_device_train_batch_size 64 \
+    --gradient_accumulation_steps 1 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 1e-6 \
+    --num_train_epochs 5.0 \
+    --plot_loss \
+    --quantization_bit 4 \
+    &> $OUTPUT/training.log
