bbd8e167c9152efd085250b34e947800cd969811
dw <1798827828@qq.com>
2023-12-24 22:11:41 -0500

    fix

--- src/apr_rm.sh
+++ src/apr_rm.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 OUTPUT='/projects/ksun3/dwu25/trained_models/apr_rm/'
-mkdir -p $OUTPUT
 
 accelerate launch --config_file accelerate_config.yaml train_bash.py \
     --stage rm \
     --do_train \
     --model_name_or_path codellama/CodeLlama-7b-hf \
-    --adapter_name_or_path $OUTPUT \
+    --adapter_name_or_path $OUTPUT/adapter \
     --dataset apr_rm \
     --dataset_dir /projects/ksun3/dwu25/apr_datasets_processing/coconut/data \
     --template default \
     --finetuning_type lora \
     --lora_target q_proj,v_proj \
     --lora_rank 32 \
-    --output_dir $OUTPUT \
+    --output_dir $OUTPUT/output \
     --per_device_train_batch_size 64 \
     --gradient_accumulation_steps 1 \
     --lr_scheduler_type cosine \
