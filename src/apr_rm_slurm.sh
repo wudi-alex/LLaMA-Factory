036d964e8d0518fb5928ce6f6fcc88f4a35a3b3a
dw <1798827828@qq.com>
2024-01-01 22:06:48 -0500

    fix

--- src/apr_rm_slurm.sh
+++ src/apr_rm_slurm.sh
@@ -32,17 +32,17 @@ accelerate launch --config_file accelerate_ds_config.yaml train_bash.py \
     --model_name_or_path codellama/CodeLlama-7b-hf \
     --dataset apr_rm \
     --dataset_dir /projects/ksun3/dwu25/apr_datasets_processing/coconut/data \
-    --template default \
+    --template vanilla \
     --finetuning_type lora \
     --lora_target q_proj,v_proj \
-    --lora_rank 16 \
+    --lora_rank 8 \
     --output_dir $OUTPUT/output \
-    --per_device_train_batch_size 32 \
+    --per_device_train_batch_size 64 \
     --gradient_accumulation_steps 1 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
     --save_steps 1000 \
     --learning_rate 5e-6 \
-    --num_train_epochs 5.0 \
+    --num_train_epochs 3.0 \
     --plot_loss \
     --quantization_bit 4 \
